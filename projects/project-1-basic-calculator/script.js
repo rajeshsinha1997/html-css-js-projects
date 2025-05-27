a40fa1c804eee5971a52557a98ce64bd7367d507
Rajesh Sinha <rajeshsinha1997@gmail.com>
2025-05-27 16:14:07 +0530

    fixed minor bugs

--- projects/project-1-basic-calculator/script.js
+++ projects/project-1-basic-calculator/script.js
@@ -8,13 +8,15 @@ const buttonElements = document.querySelectorAll("button");
 let needToClearScreen = true;
 let previousOperand = "";
 let operatorToBeUsed = "";
+let lastPressedButtonWasOperator = true;
 
 /**
  * function to reset the calculator
  */
-let resetCalculator = () => {
+const resetCalculator = () => {
   // reset everything
   needToClearScreen = true;
+  lastPressedButtonWasOperator = true;
   previousOperand = "";
   operatorToBeUsed = "";
   inputBoxElement.value = "0";
@@ -23,7 +25,7 @@ let resetCalculator = () => {
 /**
  * function to delete the last character of the existing input box value
  */
-let deleteLastCharacter = () => {
+const deleteLastCharacter = () => {
   // check if the length of the existing value of the input box is greater than 1
   if (inputBoxElement.value.length > 1) {
     // remove the last character
@@ -34,6 +36,9 @@ let deleteLastCharacter = () => {
   } else {
     // set value of the input box to 0
     inputBoxElement.value = "0";
+
+    // update flag to clear screen on next non-operator button press
+    needToClearScreen = true;
   }
 };
 
@@ -41,54 +46,65 @@ let deleteLastCharacter = () => {
  * function to set a value to the input box element
  * @param {string} newValue - value to be set to the input box element
  */
-let setValueToInputBox = (newValue) => {
-  // check if existing value of the input box is 0 or 00
+const setValueToInputBox = (newValue) => {
+  // check if the screen needed to be cleared
+  // or the existing value is '0' or '00'
   if (
+    needToClearScreen ||
     inputBoxElement.value === "0" ||
-    inputBoxElement.value === "00" ||
-    needToClearScreen
+    inputBoxElement.value === "00"
   ) {
-    // replace the existing value by the new value
+    // clear the screen and place the new value on the screen
     inputBoxElement.value = newValue;
   } else {
+    // check if the new value is a period and a period has been added already
+    if (newValue === "." && inputBoxElement.value.includes(".")) return;
+
     // append the new value to the existing value
     inputBoxElement.value += newValue;
   }
 };
 
 /**
+ * function to perform any pending calculation and store the result
+ * as previous operand
+ */
+const performPendingCalculation = () => {
+  // evaluate the result and store as the previous operand
+  previousOperand = eval(
+    previousOperand + operatorToBeUsed + inputBoxElement.value
+  );
+};
+
+/**
  * function to perform required actions when any operator button is clicked
  * @param {string} operatorValue - value of the operator
  */
-let setOperator = (operatorValue) => {
-  // check if any operator has been set already
-  if (operatorToBeUsed !== "") {
-    // perform pending calculation
-    previousOperand = eval(
-      previousOperand + operatorToBeUsed + inputBoxElement.value
-    );
-
-    // reset operator
-    operatorToBeUsed = "";
-  } else {
-    // store value from the input box element
+const setOperator = (operatorValue) => {
+  // check if the previous operand value is empty
+  if (previousOperand === "") {
+    // store the value present on the screen as previous operand
     previousOperand = inputBoxElement.value;
+  } else {
+    // check if the last button pressed was not an operator
+    if (!lastPressedButtonWasOperator) {
+      // perform pending calculation
+      performPendingCalculation();
+    }
   }
 
-  // update operator value
+  // update operator to be used
   operatorToBeUsed = operatorValue;
 };
 
 /**
  * function to display the result to the input box element
  */
-let displayResult = () => {
+const displayResult = () => {
   // check if any calculation is pending
   if (operatorToBeUsed !== "" && !needToClearScreen) {
-    // perform the pending calculation
-    previousOperand = eval(
-      previousOperand + operatorToBeUsed + inputBoxElement.value
-    );
+    // perform pending calculation
+    performPendingCalculation();
 
     // reset operator
     operatorToBeUsed = "";
@@ -96,10 +112,14 @@ let displayResult = () => {
 
   // display result to the input box element
   inputBoxElement.value = previousOperand;
+
+  // reset previous operand value
+  previousOperand = "";
 };
 
 // add event listener to each button for the 'click' event
 buttonElements.forEach((button) => {
+  // handle mouse click events
   button.addEventListener("click", (event) => {
     // get button text
     const buttonText = event.target.textContent;
@@ -108,12 +128,11 @@ buttonElements.forEach((button) => {
     switch (buttonText) {
       case "AC":
         resetCalculator();
+        lastPressedButtonWasOperator = true;
         break;
       case "DEL":
         deleteLastCharacter();
-        break;
-      case "00":
-        setValueToInputBox("00");
+        lastPressedButtonWasOperator = true;
         break;
       case ".":
       case "0":
@@ -126,7 +145,9 @@ buttonElements.forEach((button) => {
       case "7":
       case "8":
       case "9":
+      case "00":
         setValueToInputBox(buttonText);
+        lastPressedButtonWasOperator = false;
         needToClearScreen = false;
         break;
       case "%":
@@ -135,16 +156,45 @@ buttonElements.forEach((button) => {
       case "-":
       case "+":
         setOperator(buttonText);
+        lastPressedButtonWasOperator = true;
         needToClearScreen = true;
         break;
       case "=":
         displayResult();
+        lastPressedButtonWasOperator = true;
         break;
     }
 
     // debug statements
-    // console.log(`need to clear screen: ${needToClearScreen}`);
-    // console.log(`previous operand: ${previousOperand}`);
-    // console.log(`operator to be used: ${operatorToBeUsed}`);
+    console.log(
+      `last button pressed was an operator: ${lastPressedButtonWasOperator}`
+    );
+    console.log(`need to clear screen: ${needToClearScreen}`);
+    console.log(`previous operand: ${previousOperand}`);
+    console.log(`operator to be used: ${operatorToBeUsed}`);
   });
+
+  // handle keyboard input
+  // document.addEventListener("keydown", (e) => {
+  //   const key = e.key;
+  //   const allowedKeys = "0123456789.+-*/%";
+
+  //   // if key is a valid number or operator, input it
+  //   if (allowedKeys.includes(key)) {
+  //     setValueToInputBox(key);
+  //     needToClearScreen = false;
+  //   }
+  //   // handle Enter key for result
+  //   else if (key === "Enter") {
+  //     displayResult();
+  //   }
+  //   // handle Backspace key for deleting last character
+  //   else if (key === "Backspace") {
+  //     deleteLastCharacter();
+  //   }
+  //   // handle Escape key to reset calculator
+  //   else if (key === "Escape") {
+  //     resetCalculator();
+  //   }
+  // });
 });
